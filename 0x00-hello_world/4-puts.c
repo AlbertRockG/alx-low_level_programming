6cdebea2c03b9bcc00ef5f32c64c4d899f8bc498
bioaddict <rafgangbadja@gmail.com>
2022-06-03 00:15:18 +0100

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * File : 4-puts.c
+ * Author : AlbertG
+ */
+
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
