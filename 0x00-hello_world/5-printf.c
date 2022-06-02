0a8763ee4ce343fcb6d50f2a6c9a6180e9505414
bioaddict <rafgangbadja@gmail.com>
2022-06-03 00:29:22 +0100

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File : 5-printf.c
+ * Author : AlbertG
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is
+ * a piece of art,, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
