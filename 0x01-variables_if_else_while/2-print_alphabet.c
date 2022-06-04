a80460b5e6baccd6a44878cffde371ecbc43b693
bioaddict <rafgangbadja@gmail.com>
2022-06-04 03:50:16 +0100

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+ * File: 2-print_alphabet.c
+ * Author: AlbertG
+ */
+
+/**
+ * main - Prints the alphabet in lowercase,
+ * followed by a new line with the putchar
+ * function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char mychar;
+
+	for (mychar = 'a'; mychar <= 'z'; ++mychar)
+	{
+		putchar(mychar);
+	}
+	putchar('\n');
+
+	return (0);
+}
