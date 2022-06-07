2578f5326e9bbff0a1ec432becfcb651ed43d704
bioaddict <rafgangbadja@gmail.com>
2022-06-07 19:28:02 +0100

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# Functions and nested loop
+
+
+## `_putchar` a program that prints `_puchar`, followed by new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: AlbertG
+ * Desc: Header file containing declarations
+ * for all functions
+ * used in the 0x02-functions_nested_loops
+ * directory
+ */
+
+int _putchar(char c);
+
+#endif
