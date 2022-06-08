0040f2db9325328ee13af3ad731b630ce541b28e
bioaddict <rafgangbadja@gmail.com>
2022-06-08 03:52:43 +0100

    Add 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,9 @@ void jack_bauer(void)
 {
 	int hours, minutes;
 
-	for (hours = 0; hours < 23; hours++)
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for (minutes = 0; minutes < 59; minutes++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
