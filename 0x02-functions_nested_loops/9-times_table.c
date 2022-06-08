9459428748470b508088e319356ee29d53e21cb7
bioaddict <rafgangbadja@gmail.com>
2022-06-09 00:50:20 +0100

    Add 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+/*
+ * File: 9-times_table.c
+ * Author: AlbertG
+ */
+
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int i, j, z;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar('0');
+		j = 0;
+
+		while (j < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			z = j * i;
+
+			if (z <= 9)
+				_putchar(' ');
+			else
+				_putchar((z / 10) + '0');
+
+			_putchar((z % 10) + '0');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
