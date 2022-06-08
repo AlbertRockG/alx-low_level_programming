e17b9b8ee2b546de7333d5abee5a4b16d3466876
bioaddict <rafgangbadja@gmail.com>
2022-06-08 03:50:08 +0100

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+/*
+ * File: 8-24_hours.c
+ * Author: AlbertG
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ * prototype: void jack_bauer(void);
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 23; hours++)
+	{
+		for (minutes = 0; minutes < 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
