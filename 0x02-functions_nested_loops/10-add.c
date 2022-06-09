07ece24a4822881e1b7854907219a17aee25f97f
bioaddict <rafgangbadja@gmail.com>
2022-06-09 01:02:59 +0100

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * File: 10-add.c
+ * Author: AlbertG
+ */
+
+/**
+ * add - adds two integers.
+ * @a: first integer to add.
+ * @b: second integer to add.
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
