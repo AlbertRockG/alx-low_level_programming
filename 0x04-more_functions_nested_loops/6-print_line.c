d2eca13e80c427135f01736f409abee5f17e8999
bioaddict <rafgangbadja@gmail.com>
2022-06-10 03:49:16 +0100

    Draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+/*
+ * File: 6-print_line.c
+ * Author: AlbertG
+ */
+
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: number of times
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
