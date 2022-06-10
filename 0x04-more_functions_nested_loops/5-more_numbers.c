8854b103da355dbbe69cf61c954149fc3d1d99e3
bioaddict <rafgangbadja@gmail.com>
2022-06-10 03:31:19 +0100

    Prints more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+/*
+ * File: 5-more_numbers.c
+ * Author: AlbertG
+ */
+
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int times, number;
+
+	times = 1;
+
+	while (times < 10)
+	{
+		number = 0;
+
+		while (number < 15)
+		{
+			if (number > 9)
+			{
+				_putchar((number / 10) + '0');
+			}
+			_putchar((number % 10) + '0');
+			number++;
+		}
+		_putchar('\n');
+		times++;
+	}
+}
