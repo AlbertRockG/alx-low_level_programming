1212aff04ff07e763035ad49e48dcd266542bc29
bioaddict <rafgangbadja@gmail.com>
2022-06-11 03:36:48 +0100

    Print the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+/*
+ * File: 100-prime_factor.c
+ * Author: AlbertG
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the largest prime
+ *        factor of the number 612852475143.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long prime = 612852475143, divisor;
+
+	while (divisor < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (divisor = 3; divisor < (prime / 2); divisor += 2)
+		{
+			if ((prime % divisor) == 0)
+				prime /= divisor;
+		}
+	}
+
+	printf("%ld\n", prime);
+
+	return (0);
+}
