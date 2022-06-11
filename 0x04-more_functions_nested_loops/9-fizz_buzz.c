edde5f15584cc3e89b95e70f0f4e57873c2a4827
bioaddict <rafgangbadja@gmail.com>
2022-06-11 03:16:48 +0100

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 	for (number = 1; number < 101; number++)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
-			printf("Fizz Buzz");
+			printf("FizzBuzz");
 
 		else if (number % 3 == 0)
 			printf("Fizz");
