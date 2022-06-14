82ee17b3b97be65843354851fddd84ed1b04c9c6
bioaddict <rafgangbadja@gmail.com>
2022-06-14 23:24:39 +0000

    Reset a variable value

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: AlbertG
+ * Desc: Header file containing declarations
+ * for all functions
+ * used in the 0x04-more_functions_nested_loops
+ * directory
+ */
+
+void reset_to_98(int *n);
+
+#endif
