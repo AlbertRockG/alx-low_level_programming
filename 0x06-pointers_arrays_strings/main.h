e8d3acd0f97ba24c4bbebb8b8f9778d262b12e5d
bioaddict <rafgangbadja@gmail.com>
2022-06-16 23:44:23 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: AlbertG
+ * Desc: Header file containing declarations
+ * for all functions
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
