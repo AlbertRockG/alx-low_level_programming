20cf83104e6befb47db9841d9b3593323fce0eac
bioaddict <rafgangbadja@gmail.com>
2022-06-17 01:40:42 +0000

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - Copies at most an inputted number of bytes for src to dest.
+ * @dest: The destination string.
+ * @src: The source string to be concatenated to @dest.
+ * @n: The maximum number of bytes to be copied.
+ *
+ * Return: a pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+	char *cpdest = dest;
+
+	while (cpdest[dest_len] != '\0')
+		dest_len++;
+
+	while (n--)
+		dest[dest_len++] = src[index++];
+
+
+	dest[dest_len++] = '\0';
+
+	return (dest);
+}
