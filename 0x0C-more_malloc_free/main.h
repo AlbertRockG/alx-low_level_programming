7eee768940fcf1eed603c9f51e859f0c84be35cb
bioaddict <rafgangbadja@gmail.com>
2022-06-30 16:38:19 +0000

    Concatenates two strings

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: AlbertG
+ * Desc: Header file containing declarations
+ * for all functions
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void _realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
