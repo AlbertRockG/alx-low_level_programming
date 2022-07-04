dca65ee9e0e14977ec5ea01c3dddc1d3f60aab59
bioaddict <rafgangbadja@gmail.com>
2022-07-04 10:39:10 +0000

    Defines a macro named PI as an abbreviation to the value of pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.H
+ * Author: Albert G
+ * Desc: Header file that defines a macro named
+ *       PI as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif /* #define PI_H */
