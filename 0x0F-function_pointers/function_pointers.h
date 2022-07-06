1cf038e3b61968027b5b206e33cace796f40af80
bioaddict <rafgangbadja@gmail.com>
2022-07-06 15:49:15 +0000

    A function that prints a name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: main.h
+ * Author: AlbertG
+ * Desc: Header file containing declarations
+ * for all functions
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /* #define FUNCTION_POINTERS_H */
