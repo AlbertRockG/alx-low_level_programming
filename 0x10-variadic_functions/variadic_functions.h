e1adba71ead96b785c6d92c9943fecda93c545f3
bioaddict <rafgangbadja@gmail.com>
2022-07-07 17:22:02 +0000

    A function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,21 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * struct printer - A new struct type defining a printer.
+ * @formater: A formating symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
+typedef struct printer
+{
+	char *formater;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
