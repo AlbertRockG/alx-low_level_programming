b95c11e66743fc3dbcee67bdbfede726b22eeb0b
bioaddict <rafgangbadja@gmail.com>
2022-09-02 00:42:04 +0100

    Fix the error

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
  */
 
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
  * @head: A pointer to the head of the dlistint_t list.
